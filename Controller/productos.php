e74f5965c21df066bb35b024f3a77a4cb640d87a
gerardobaldizon <gerardobaldizon@gmail.com>
2022-10-16 22:25:15 -0600

    creados de udemy

--- Controller/productos.php
+++ Controller/productos.php
@@ -1,6 +1,18 @@
 <?php
     require_once("../config/conexion.php");
     require_once("../Model/productos.php");
+    $Productos = new Productos();
+
+    switch($_GET["OP"]){
+
+        case "listar":
+            $datos=$Productos->get_producto();
+            $data=Arras();
+
+
+
+
+    }
 
 
 ?>
