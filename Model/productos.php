5f7effceb8b360017b3998a2d203e446e075499b
gerardobaldizon <gerardobaldizon@gmail.com>
2022-10-22 11:17:22 -0600

    revision de vairales

--- Model/productos.php
+++ Model/productos.php
@@ -6,37 +6,38 @@
             $sql = $conectar->prepare($sql);
             $sql-> execute();
             return $resultado =$sql->fetchAll();
+        }
 
-            public function get_producto_id($id_){
-                $conectar = parent::conexion();
-                $sql = "Select * from crud where id_=?";
-                $sql = $conectar->prepare($sql);
-                $sql-> bindvalue(1,$prod_id);
-                $sql-> execute();
-                return $resultado =$sql->fetchAll();
-            
-            public function delete_producto_id($id_){
-                $conectar = parent::conexion();
-                $sql = "update crud set Activo=0, where id_=?";
-                $sql = $conectar->prepare($sql);
-                $sql-> bindvalue(1,$prod_id);
-                $sql-> execute();
-                return $resultado =$sql->fetchAll();
-
-            public function insertar_producto($pro_nombre){
-                $conectar = parent::conexion();
-                $sql = "INSERT INTO proveedores (id_, NIT, Razón social, Nombre comercial, Activos) VALUES (NULL, '?', '?', '?', 1);";
-                $sql = $conectar->prepare($sql);
-                $sql-> bindvalue(1,$pro_nombre);
-                $sql-> execute();
-                return $resultado =$sql->fetchAll();
-
-            public function update_producto_id($id_){
-                $conectar = parent::conexion();
-                $sql = "update crud set NIT=0, where id_=?";
-                $sql = $conectar->prepare($sql);
-                $sql-> bindvalue(1,$prod_id);
-                $sql-> execute();
-                return $resultado =$sql->fetchAll();
+        public function get_producto_id($prod_id){
+            $conectar = parent::conexion();
+            $sql = "Select * from crud where id_=?";
+            $sql = $conectar->prepare($sql);
+            $sql-> bindvalue(1,$prod_id);
+            $sql-> execute();
+            return $resultado =$sql->fetchAll();
+        }    
+        public function delete_producto_id($prod_id){
+            $conectar = parent::conexion();
+            $sql = "update crud set Activo=0, where id_=?";
+            $sql = $conectar->prepare($sql);
+            $sql-> bindvalue(1,$prod_id);
+            $sql-> execute();
+            return $resultado =$sql->fetchAll();
+        }
+        public function insertar_producto($pro_nombre){
+            $conectar = parent::conexion();
+            $sql = "INSERT INTO proveedores (id_, NIT, Razón social, Nombre comercial, Activos) VALUES (NULL, '?', '?', '?', 1);";
+            $sql = $conectar->prepare($sql);
+            $sql-> bindvalue(1,$pro_nombre);
+            $sql-> execute();
+            return $resultado =$sql->fetchAll();
+        }
+        public function update_producto_id($prod_id){
+            $conectar = parent::conexion();
+            $sql = "update crud set NIT=0, where id_=?";
+            $sql = $conectar->prepare($sql);
+            $sql-> bindvalue(1,$prod_id);
+            $sql-> execute();
+            return $resultado =$sql->fetchAll();
         }
     }
